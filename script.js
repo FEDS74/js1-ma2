e37e503aebe337213e19e78c4ffa74447f202fa1
Thomas Bach-Nilsen <thni@hesselberg.no>
2020-02-11 14:33:55 +0100

    new

---
+++ script.js
@@ -0,0 +1,34 @@
+//question and answer 1
+var myFunctionExpression = function(){
+  console.log("Thomas Bach-Nilsen");
+};
+
+myFunctionExpression();
+
+//question and answer 2
+const callBackClick = document.querySelector(".btn");
+callBackClick.addEventListener("click", function(){
+  console.log("I was clicked");
+});
+
+//question and answer 3
+const inputFirstName = document.querySelector("#firstName");
+inputFirstName.addEventListener("keydown", function(){
+});
+inputFirstName.addEventListener("keyup", function(event){
+  console.log("The value of key is: ", event.target.value);
+ });
+
+//question and answer 4
+const mouseoverButton = document.querySelector("button");
+mouseoverButton.addEventListener("mouseover", function() {
+  this.classList.add("hover");
+});
+
+//question and answer 5
+const mouseoutButton = document.querySelector("[data-animal]");
+mouseoutButton.addEventListener("mouseout", function() {
+this.classList.remove("hover");
+});
+
+// question and answer 6
